0f0beffe3449752c45448f506bd4455158e5c328
Mayra Carrión <61747592+mayracarrion96@users.noreply.github.com>
2020-03-10 11:07:04 -0500

    declarar variables
    declarar

---
+++ Tipodedatos/src/tipodedatos/Tipodedatos.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipodedatos;
+
+/**
+ *
+ * @author usuario
+ */
+public class Tipodedatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    //declara una nuemero variable
+    // enteros -32768 a 32767
+    int numeroEntero = 0;
+    double numeroDouble= 0.0;
+    char letra ='A';
+    String cadena="hola mundi";
+    boolean VoF= false;
+    //para cambiar un valor
+    valorBool=true;
+    
+    
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    // comentario de una sola linea
+    /* comentario
+    de un
+    parrafo o ms lineas
+    */
+    
+}
